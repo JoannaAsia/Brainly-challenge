494c6e8e374b2fb1385b686a63bd0dbb0b4cb891
JoannaAsia <joanna.wojcik91@gmail.com>
2017-05-02 18:23:34 +0200

    application is functional

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 //load the external JSON
 var xhr = new XMLHttpRequest(),
 	urlJSON = 'https://cdn.rawgit.com/kdzwinel/cd08d08002995675f10d065985257416/raw/811ad96a0567648ff858b4f14d0096ba241f28ef/quiz-data.json';
+
 xhr.open('GET', urlJSON);
 xhr.send(null);
 
@@ -9,37 +10,33 @@ xhr.onreadystatechange = function () {
   var OK = 200;
   if (xhr.readyState === DONE) {
     if (xhr.status === OK) {
-      startQuiz();
+      openQuiz();
     } else {
       console.log('Error: ' + xhr.status);
     }
   }
 };
 
-
-
-// create a start button
-function startQuiz() {
+// create a start quiz function
+function openQuiz() {
 	var startButton = document.querySelector('.start-button');
 
 	startButton.addEventListener('click', function() {
- 	// e.stopPropagation();
  	startButton.classList.add('hidden');
 
-    var fiveMinutes = 300, 
+    var quizTime = 300, 
         displayTimer = document.querySelector('.timer'),
         quizContent = document.querySelector('.quiz-content');
 
     displayTimer.classList.remove('hidden');
     quizContent.classList.remove('hidden');
 
-    startTimer(fiveMinutes, displayTimer);
+    // call all needed functions
+    startTimer(quizTime, displayTimer);
     displayData();
     submitQuiz();
-
 	});
 }
-
 
 // create a submit quiz function
 function submitQuiz() {
@@ -51,33 +48,46 @@ function submitQuiz() {
 	quizContent.appendChild(submitButton);
 
 	submitButton.addEventListener('click', function() {
-		getSelectedAnswers();
+		evaluateScore();
+		submitButton.classList.add('hidden');
 	});
-
-	// stop the quiz
-
-	// display a score
 }
 
-
 // create  a function that gets all selected answers
-function getSelectedAnswers() {
+function evaluateScore() {
 	var allAnswers = document.querySelectorAll('input[type=radio]'),
 		score = 0;
 
 	for(var k = 0; k < allAnswers.length; k++) {
 		if(allAnswers[k].checked) {
-			// check if answers are correct
-			console.log(allAnswers[k].checked); // returns true
-			if(allAnswers[k].value == true) {
+			if(allAnswers[k].value == "true") {
 				score++;
-				// todo: this is doesn't work
 			}
 		}
 	}
+	// create feedback for the user
+	var evaluation = document.createElement('footer'),
+	    displayScore = document.createElement('h2'),
+	    displayScoreDesc = document.createElement('p');
+
+
+	displayScore.innerHTML = score + '/9';
+
+	if(score < 4) {
+		displayScoreDesc.innerHTML = 'Nie masz co się wstydzić... czas zabrać się do pracy!';
+	}
+	if(score >= 4) {
+		displayScoreDesc.innerHTML = 'Całkiem nieźle mistrzu, ale stać Cię na więcej!';
+	}
+	if(score > 7) {
+		displayScoreDesc.innerHTML = 'Świetna robota!!!';
+	}
+
+	var mainContent = document.getElementById('main-content');
+	mainContent.appendChild(evaluation);
+	evaluation.appendChild(displayScore);
+	evaluation.appendChild(displayScoreDesc);
 }
-
-
 
 // display JSON Data
 function displayData() {
@@ -85,7 +95,7 @@ function displayData() {
 	    resJSON = JSON.parse(res),
         quizContent = document.querySelector('.quiz-content'),
 		questions = resJSON.questions,
-	    questionsNum = questions.length, // 9
+	    questionsNum = questions.length, 
         answersNum;
 
 
@@ -93,8 +103,7 @@ function displayData() {
 	var questionList = document.createElement('ol');
 	quizContent.appendChild(questionList);
 
-
-	// get questions
+	// display questions
 	for(var i = 0; i < questionsNum; i++) {
 		var questionListElem = document.createElement('li');
 		var questionText = document.createElement('p');
@@ -103,10 +112,11 @@ function displayData() {
 		questionListElem.appendChild(questionText);
 		questionList.appendChild(questionListElem);
 
-		// get answers
+		// display answers
 		answersNum = questions[i].answers.length; //4
 		for(var j = 0; j < answersNum; j++) {
 			var inputValue = questions[i].answers[j].correct;
+
 			// create label and input elements for answers
 			var label = document.createElement('label');
 			var input = document.createElement('input');
@@ -118,9 +128,7 @@ function displayData() {
 			label.insertBefore(input, label.firstChild);
 			questionListElem.appendChild(label);
 		}
-
 	}
-
 }
 
 // create a timer function
@@ -128,20 +136,21 @@ function startTimer(duration, displayTimer) {
     var minutes, 
         seconds;
 
-    setInterval(function () {
+    var intervalId = setInterval(function () {
         minutes = parseInt(duration / 60, 10);
         seconds = parseInt(duration % 60, 10)
         seconds = seconds < 10 ? "0" + seconds : seconds;
         displayTimer.textContent = minutes + ":" + seconds;
 
         if (--duration < 0) {
-            displayTimer.textContent = 'Your time is up!!!';
-            return;
-        }
+        displayTimer.textContent = 'Your time is up!!!';
+        stopTimer(intervalId);
+    }
 
     }, 1000);
 }
 
-
-
+function stopTimer(interval) {
+	clearInterval(interval);
+};
 
