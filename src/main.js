52b945ca571da2a136e57327cdebd62ff97b29c5
JoannaAsia <joanna.wojcik91@gmail.com>
2017-05-02 14:13:27 +0200

    JavaScript gets selected answers

--- src/main.js
+++ src/main.js
@@ -9,13 +9,75 @@ xhr.onreadystatechange = function () {
   var OK = 200;
   if (xhr.readyState === DONE) {
     if (xhr.status === OK) {
-      displayData();
-      findAllInputs();
+      startQuiz();
     } else {
       console.log('Error: ' + xhr.status);
     }
   }
 };
+
+
+
+// create a start button
+function startQuiz() {
+	var startButton = document.querySelector('.start-button');
+
+	startButton.addEventListener('click', function() {
+ 	// e.stopPropagation();
+ 	startButton.classList.add('hidden');
+
+    var fiveMinutes = 300, 
+        displayTimer = document.querySelector('.timer'),
+        quizContent = document.querySelector('.quiz-content');
+
+    displayTimer.classList.remove('hidden');
+    quizContent.classList.remove('hidden');
+
+    startTimer(fiveMinutes, displayTimer);
+    displayData();
+    submitQuiz();
+
+	});
+}
+
+
+// create a submit quiz function
+function submitQuiz() {
+    var quizContent = document.querySelector('.quiz-content')
+	// create a submit button
+	var submitButton = document.createElement('button');
+	submitButton.setAttribute('type', 'submit');
+	submitButton.innerHTML = 'Submit';
+	quizContent.appendChild(submitButton);
+
+	submitButton.addEventListener('click', function() {
+		getSelectedAnswers();
+	});
+
+	// stop the quiz
+
+	// display a score
+}
+
+
+// create  a function that gets all selected answers
+function getSelectedAnswers() {
+	var allAnswers = document.querySelectorAll('input[type=radio]'),
+		score = 0;
+
+	for(var k = 0; k < allAnswers.length; k++) {
+		if(allAnswers[k].checked) {
+			// check if answers are correct
+			console.log(allAnswers[k].checked); // returns true
+			if(allAnswers[k].value == true) {
+				score++;
+				// todo: this is doesn't work
+			}
+		}
+	}
+}
+
+
 
 // display JSON Data
 function displayData() {
@@ -44,13 +106,13 @@ function displayData() {
 		// get answers
 		answersNum = questions[i].answers.length; //4
 		for(var j = 0; j < answersNum; j++) {
+			var inputValue = questions[i].answers[j].correct;
 			// create label and input elements for answers
 			var label = document.createElement('label');
-
 			var input = document.createElement('input');
 			input.type = 'radio';
 			input.name = 'q'+[i+1];
-			input.value = input.name+'answer'+[j+1];
+			input.value = inputValue;
 
 			label.innerHTML = questions[i].answers[j].answer;
 			label.insertBefore(input, label.firstChild);
@@ -59,62 +121,25 @@ function displayData() {
 
 	}
 
-	// create a submit button
-	var submitButton = document.createElement('button');
-	submitButton.setAttribute('type', 'submit');
-	submitButton.innerHTML = 'Submit';
-	quizContent.appendChild(submitButton);
-
 }
 
-// create a timer
+// create a timer function
 function startTimer(duration, displayTimer) {
-    var timer = duration, minutes, seconds;
+    var minutes, 
+        seconds;
+
     setInterval(function () {
-        minutes = parseInt(timer / 60, 10);
-        seconds = parseInt(timer % 60, 10);
-        
+        minutes = parseInt(duration / 60, 10);
+        seconds = parseInt(duration % 60, 10)
         seconds = seconds < 10 ? "0" + seconds : seconds;
-
-
         displayTimer.textContent = minutes + ":" + seconds;
 
-        if (--timer < 0) {
+        if (--duration < 0) {
             displayTimer.textContent = 'Your time is up!!!';
+            return;
         }
+
     }, 1000);
-}
-// create a start button
-var startButton = document.querySelector('.start-button');
-
-startButton.addEventListener('click', function (e) {
- 	e.stopPropagation();
- 	startButton.classList.add('hidden');
-
-    var fiveMinutes = 300, 
-        displayTimer = document.querySelector('.timer'),
-        quizContent = document.querySelector('.quiz-content');
-
-    displayTimer.classList.remove('hidden');
-    quizContent.classList.remove('hidden');
-
-    startTimer(fiveMinutes, displayTimer);
-});
-
-
-// find all inputs
-function findAllInputs() {
-	var allInputs = document.querySelectorAll('input[type=radio]');
-
-	for(var k = 0; k <allInputs.length; k++) {
-		console.log(allInputs[k]);
-		allInputs[k].addEventListener('click', function() {
-			
-			console.log(allInputs[k]);
-
-			// why undefined???
-		});
-	}
 }
 
 
